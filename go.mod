cad98d43243487f760e3f06c0263272847f4fef7
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-04 04:20:47 +0900

    go mod init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/richardimaoka/go-cli-sandbox
+
+go 1.18
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "os/exec"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
-    exec.Command("")
-    fmt.Println("gloと入力して下さい。")
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        if scanner.Text() == "glo" {
-	    out, err := exec.Command("glo").Output()
-	    if err != nil {
-		fmt.Println("ERROR occurred")
-		fmt.Println(err)
-	    }
-	    fmt.Println("SUCCESS")
-	    fmt.Println(out)
-            break
-        }
-        fmt.Println("git status と入力されるまでループします。")
-    }
-    fmt.Println("終了します。")
+	exec.Command("")
+	fmt.Println("gloと入力して下さい。")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "glo" {
+			out, err := exec.Command("sh -c glo").Output()
+			if err != nil {
+				fmt.Println("ERROR occurred")
+				fmt.Println(err)
+			}
+			fmt.Println("SUCCESS")
+			fmt.Println(out)
+			break
+		}
+		fmt.Println("git status と入力されるまでループします。")
+	}
+	fmt.Println("終了します。")
 }
