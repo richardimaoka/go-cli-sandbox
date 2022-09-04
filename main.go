d5f6d1f68f1b6a0b1b796353887c77b5490f4d0d
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-04 15:32:08 +0900

    json

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-func main() {
-	exec.Command("")
-	fmt.Println("gloと入力して下さい。")
+type Command struct {
+	Command string
+}
 
+func readFromStdIo() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		if scanner.Text() == "glo" {
@@ -26,4 +28,19 @@ func main() {
 		fmt.Println("git status と入力されるまでループします。")
 	}
 	fmt.Println("終了します。")
+}
+
+func main() {
+	data, err := os.ReadFile("commands.json")
+	if err != nil {
+		fmt.Println("ERROR reading commands.json")
+	}
+
+	var command []Command
+	err = json.Unmarshal(data, &command)
+	if err != nil {
+		fmt.Println("json.Unmarshal error")
+	}
+	fmt.Println(command)
+
 }
